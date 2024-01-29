e3e7fafc7ab44b33ea8a5ae0a5e2abbb787b33de
Vladikad <158090928+Vladikad@users.noreply.github.com>
2024-01-29 22:25:13 +0300

    Add files via upload

---
+++ About.php
@@ -0,0 +1,175 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Иерархия</title>
+<meta charset="utf-8">
+<meta name="description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, about">
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="RPCS3 - About"/>
+<meta property="og:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering."/>
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/1200.png"/>
+<meta property="og:image:width" content="1200"/>
+<meta property="og:image:height" content="630"/>
+<meta property="og:url" content="https://rpcs3.net"/>
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website"/>
+<meta property="og:site_name" content="RPCS3"/>
+<meta name="twitter:title" content="RPCS3 - About">
+<meta name="twitter:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>Иерархия</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Важно чтобы в составе было понимание кто к кому может обратиться по определенному вопросу, на который сам не знает ответа, или нужен доступ.
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Игроки</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Желательно чтобы игроки обращались в приоритете к младшему составу, в основном к помощникам. Так Помощник будет набираться опыта в помощи, разгружать работу Старшего Состава.  <br>
+									<br>
+									 Если нет помощников, модераторов или вопрос был задан конкретно члену Старшего Состава, то он может быть проигнорирован, и ответить можно по желанию или возможности.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/project.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-img-container splitter-con-left'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/history.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Младший состав</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Младший состав
+
+Самый частый камень преткновения это обращения младшего состава.
+
+Если Стажёр не знает как поступить, то может спросить Помощника, Модератора, Старшего модератора или Главного модератора.
+
+Если Помощник не знает как поступить, то может спросить Модератора, Старшего модератора или Главного модератора.
+
+Если Модератор не знает как поступить, то может спросить Старшего модератора или Главного модератора
+
+<aside>
+Младший состав не должен бегать спрашивать Куратора или Администратора по вопросам с которым справится Старший Состав сервера
+
+</aside> <br>
+									<br>
+									Все вопросы касательно игрового процесса должны проходить через Старший Состав. 
+
+Исключение: Если вас не устраивает работа кого-то из Старшего состава, то вы можете обратиться к Куратору. 
+Исключение 2: Если вы сидите в голосовом чате вместе с Куратором или Администратором, то можете посоветоваться
+>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Причины</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 У каждого участника Состава Проекта есть своя работа. И когда в число прочего добавляется еще дополнительная нагрузка, в виде вопроса, которым должен заниматься другой, то становится сложнее.
+                                     Также вы таким образом подставляете, тех кто на одну-две должности выше вас. <br>
+									<br>
+									 Internal team members aren't the only contributors. A lot of the progress made in making PlayStation 3 emulation possible on PC is thanks to the many developers we've received contributions from in our GitHub community. While many developers do come and go, we do welcome anyone and everyone who is interested in improving the codebase.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/team.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>Также важно</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+							<aside>
+❓ Если у вас есть вопрос. То задавайте его в переписке сразу. Не нужно писать “Ты тут?” и ждать пока ответят. **Сразу введите в курс дела.**
+
+</aside>
+
+<aside>
+📵 Ни в коем случае **не нужно сразу звонить** к кому-то с вопросом без договоренности
+
+</aside>
+
+<aside>
+🚯 Если вы не в дружеских отношениях с кем-то из более старших по должности, то желательно обращаться с некоторой долей уважения к человеку и избегать панибратства.
+
+</aside>
+
+<aside>
+🚨 Тапать в Discord нужно по необходимости, а не по желанию
+
+</aside>
+						</p>
+				
